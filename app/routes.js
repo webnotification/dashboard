3191fd0edad4b4d51857940dea8974e237e98944
Ravi Kumar Nimmi <rvkmr102@gmail.com>
2016-01-06 15:12:05 +0530

    minor change

--- app/routes.js
+++ app/routes.js
@@ -177,12 +177,10 @@ module.exports = function(app, passport) {
         s3.createBucket({Bucket: s3_bucket_name}, function() {
             var params = {Bucket: s3_bucket_name, Key: req.user._id.toString(), Body: bodyStream};
             s3.putObject(params, function(err, data) {
-              if (err){       
-                    console.log(err)     
-              }
-              else{
+              if (err)
+                  console.log(err)     
+              else
                   console.log("Successfully uploaded data to myBucket/myKey");   
-              }
               res.redirect('/profile');
             });
         });
